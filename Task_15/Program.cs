72898946ef471f1bca72b7ba99b5e7e7db7bf121
Anna <zhigalovanna.28@gmail.com>
2022-11-30 20:47:00 +0300

    Add the Day of Day

---
+++ Task_15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Write("Введите трёхзначное число: ");
+int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+if (DayOfWeek == 6 || DayOfWeek == 7)
+{
+    Console.WriteLine("Да, день выходной");
+}
+else
+{
+    if (DayOfWeek < 6 && DayOfWeek > 0)
+    {
+        Console.WriteLine("Нет, это будний день");
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число!");
+    }
+}
